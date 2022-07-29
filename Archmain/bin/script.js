ab913132bbd9feef3c585d852d22fb83b8c73792
Jonathan Sanfilippo <103053714+JonathanSanfilippo@users.noreply.github.com>
2022-07-29 18:44:42 +0100

    Delete script.js

--- Archmain/bin/script.js
+++
@@ -1,6 +0,0 @@
-var reader = new FileReader();
-reader.onload = function(event) {
-	var testo = event.target.result;
-	document.getElementById("contenuto").innerHTML = testo;
-};
-reader.readAsText(file);
